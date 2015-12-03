832207549ea3f26f6388f1507cafec843a5f499d
Sereda-Fazen <fazen7@mail.ru>
2015-12-03 09:53:56 +0200

    01

--- tests/acceptance/15_CheckoutGuestCest.php
+++ tests/acceptance/15_CheckoutGuestCest.php
@@ -26,7 +26,6 @@ class CheckoutGuestCest
             $I->comment('Expected result: Go to the Payment information');
         }
 
-
         function paymentInfo (Step\Acceptance\LoginSteps $I, \Page\CheckoutGuest $guestPage) {
             $guestPage->paymentInformation('GIFT-ADFA-12NF0O');
             $I->comment('Expected result: Your order’s grand total is zero now. No need to add any more Gift code');
