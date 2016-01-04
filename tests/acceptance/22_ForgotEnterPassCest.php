80cdf87b886f2749eff78f31ada9c7a81989269e
Sereda-Fazen <fazen7@mail.ru>
2016-01-04 13:53:02 +0200

    137-8

--- tests/acceptance/22_ForgotEnterPassCest.php
+++ tests/acceptance/22_ForgotEnterPassCest.php
@@ -1,7 +1,7 @@
 <?php
 use \Step\Acceptance;
 /**
- * @group account
+ * @group checkNewPassword
  */
 class EnterPassCest {
 
@@ -13,10 +13,10 @@ class EnterPassCest {
         $I->newPass();
         $I->comment('Expected result: Your password has been updated');
     }
-    /*
+
     function deleteOldMsg(Step\Acceptance\LoginSteps $I, Page\ForgotPass $deleteMsg){
         $deleteMsg->deleteMsg();
         $I->comment('Expected result: Your message was deleted');
     }
-*/
+
 }
