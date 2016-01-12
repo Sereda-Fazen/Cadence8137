4603ff7e51d1b5c01a6dff714327e06eee1212f4
Sereda-Fazen <fazen7@mail.ru>
2016-01-12 10:54:51 +0200

    dev1-3

--- tests/acceptance/16_4_CheckoutGuestGiffCardCest.php
+++ tests/acceptance/16_4_CheckoutGuestGiffCardCest.php
@@ -9,7 +9,7 @@ class CheckoutGuestGiffCardCest
     function checkIfLittleMoneyOnGiffCard(Step\Acceptance\ItemsSteps  $I, Page\CheckoutGuestGiffCard $guestPage) {
         $I->processAddToCart();
 
-        $guestPage->paymentInformation('GIFT-ADFA-12NF0A');
+        $guestPage->paymentInformation('GIFT-ADFA-12NF0Z');
         $I->comment('Expected result: Prevent this page from creating additional dialogs');
 
     }
