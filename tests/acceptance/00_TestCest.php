00f7ebae011f841a381fb7aa0de317dcd1437a1d
Sereda-Fazen <fazen7@mail.ru>
2016-02-09 10:54:09 +0200

    instance-2

--- tests/acceptance/00_TestCest.php
+++ tests/acceptance/00_TestCest.php
@@ -5,11 +5,10 @@ use Step\Acceptance;
 class TestCest
 {
 
-    function registerSuccess(\Page\Register $registerPage, \Step\Acceptance\LoginSteps $I)
+    function homeContent(Step\Acceptance\LoginSteps $I, \Page\Home $homePage)
     {
-        $registerPage->register('alex', 'sereda', 'cadence_test@yahoo.com', '123456', '123456');
-        $I->checkExistUser();
-        $registerPage->logout();
+        $homePage->homeSlide();
+        $homePage->homePageContent();
     }
 
 
