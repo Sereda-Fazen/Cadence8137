446ed5a727e9d2872ca031be659a2bf6b8a271e6
Sereda-Fazen <fazen7@mail.ru>
2016-02-11 15:47:26 +0200

    instance-7

--- tests/acceptance/02_0_ForgotPassCest.php
+++ tests/acceptance/02_0_ForgotPassCest.php
@@ -10,4 +10,18 @@ class ForgotPassCest {
         $forgotPage->forgot('cadence_watch@yahoo.com');
         $I->comment('Expected result: If there is an account associated with cadence_watch@yahoo.com you will receive an email with a link to reset your password.');
     }
+    function enterNewPass (Step\Acceptance\LoginSteps $I)
+    {
+        $I->gMailAuth();
+        $I->comment('Expected result: Password Reset Confirmation');
+        $I->remoteWindow();
+        $I->newPass();
+    }
+/*
+    function deleteOldMsg(Step\Acceptance\LoginSteps $I, Page\ForgotPass $deleteMsg)
+    {
+        $deleteMsg->deleteMsg('cadence_watch@yahoo.com', '!1qwerty');
+        $I->comment('Expected result: Your message was deleted');
+    }
+*/
 }
--- tests/acceptance/03_1_ForgotEnterPassCest.php
+++ tests/acceptance/03_1_ForgotEnterPassCest.php
@@ -4,7 +4,7 @@ use \Step\Acceptance;
  * @group account
  */
 class EnterPassCest {
-/*
+
     function enterNewPass (Step\Acceptance\LoginSteps $I)
     {
         $I->gMailAuth();
@@ -19,7 +19,7 @@ class EnterPassCest {
         $deleteMsg->deleteMsg('cadence_watch@yahoo.com', '!1qwerty');
         $I->comment('Expected result: Your message was deleted');
     }
-*/
+
 
 
 }
