40f10ae66a7fe65191b7a83a359dcd4521ea7028
Sereda-Fazen <fazen7@mail.ru>
2016-02-11 16:06:06 +0200

    instance-8

--- tests/acceptance/03_1_ForgotEnterPassCest.php
+++ tests/acceptance/03_1_ForgotEnterPassCest.php
@@ -1,10 +1,8 @@
 <?php
 use \Step\Acceptance;
-/**
- * @group account
- */
-class EnterPassCest {
 
+class EnterPassCest {
+/*
     function enterNewPass (Step\Acceptance\LoginSteps $I)
     {
         $I->gMailAuth();
@@ -19,7 +17,7 @@ class EnterPassCest {
         $deleteMsg->deleteMsg('cadence_watch@yahoo.com', '!1qwerty');
         $I->comment('Expected result: Your message was deleted');
     }
-
+*/
 
 
 }
