9715cc0992beccb35aa997f3cdee867e7275c26f
Alex <sa@itsvit.org>
2016-03-14 16:55:56 +0200

    130-2

--- tests/_support/Step/Acceptance/LoginSteps.php
+++ tests/_support/Step/Acceptance/LoginSteps.php
@@ -31,7 +31,7 @@ class LoginSteps extends \AcceptanceTester
     public function stepsLoginIn()
     {
         $I = $this;
-        $I->amOnPage('/customer/account/login/');
+        $I->amOnPage('/default/customer/account/login/');
         try { $I->click('.closeNewsletter'); } catch (Exception $e) {}
         $I->wait(2);
         $I->fillField('#email', 'cadence_watch@yahoo.com');
