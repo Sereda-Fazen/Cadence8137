4eb89ce75d5d60412f596213be80e638238bf8ef
Alex <sa@itsvit.org>
2016-03-15 09:40:07 +0200

    130-5

--- tests/acceptance/00_TestCest.php
+++ tests/acceptance/00_TestCest.php
@@ -16,4 +16,56 @@ class TestCest
         $I->checkGiffCard();
         $I->comment('Expected result: Showing 1 products which in processing');
     }
+
+    function checkOrderIsComplete(\Step\Acceptance\AdminSteps $I, \Page\Admin $adminPanel) {
+
+        $I->loginCustomer();
+        $I->checkOrder();
+        $adminPanel->orderComplete();
+    }
+
+    function checkGiftCardForUser2(Step\Acceptance\UserSteps  $I)
+    {
+        $I->stepsLoginIn();
+        $I->comment('Expected result: You are entering in your account ');
+
+        $I->userProcessCheckout();
+        $I->comment('Expected result: Show your addresses');
+
+        $I->checkGiffCard();
+        $I->comment('Expected result: Showing 1 products which in processing');
+    }
+
+    function checkCreditMemo (\Step\Acceptance\AdminSteps $I, \Page\Admin $adminPanel) {
+
+        $I->loginCustomer();
+        $I->checkOrder();
+        $adminPanel->checkCreditMemo();
+    }
+
+
+    function checkGiftCardForUser3(Step\Acceptance\UserSteps  $I)
+    {
+        $I->stepsLoginIn();
+        $I->comment('Expected result: You are entering in your account ');
+
+        $I->userProcessCheckout();
+        $I->comment('Expected result: Show your addresses');
+
+        $I->checkGiffCard();
+        $I->comment('Expected result: Showing 1 products which in processing');
+    }
+
+    function checkShipment (\Step\Acceptance\AdminSteps $I, \Page\Admin $adminPanel) {
+
+        $I->loginCustomer();
+        $I->checkOrder();
+        $adminPanel->checkShipment('123456');
+    }
+
+    function checkExportReport (\Step\Acceptance\AdminSteps $I, \Page\Admin $adminPanel) {
+
+        $I->loginCustomer();
+        $adminPanel->checkExportReport();
+    }
 }
