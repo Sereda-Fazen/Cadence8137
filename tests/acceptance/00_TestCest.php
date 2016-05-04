e43fb7656dc3426b9fdeb5a699368bac1a530be5
Sereda-Fazen <fazen7@mail.ru>
2016-05-04 16:52:31 +0300

    dev1 -1

--- tests/acceptance/00_TestCest.php
+++ tests/acceptance/00_TestCest.php
@@ -4,14 +4,40 @@ use Step\Acceptance;
 
 class TestCest
 {
+    function loginSuccess(AcceptanceTester $I, \Page\Login $loginPage) {
+        $loginPage->login('cadence_watch@yahoo.com', '123456');
+        $I->amOnPage('/customer/account/index/');
+        $I->see('Hello, alex sereda!', 'p.hello > strong');
+        $loginPage->logout();
+    }
 
-    function addToCartPagePayPal(Step\Acceptance\ItemsSteps  $I, Page\CheckoutPayPal $guestPage) {
-        $I->processAddToCart();
+    function loginEmptyFields(AcceptanceTester $I, \Page\Login $loginPage) {
+        $loginPage->login('', '');
+        $I->see( 'This is a required field.','#advice-required-entry-pass');
+        $I->comment('Expected result: This is a required field pass and email.');
+    }
 
-        $guestPage->payPal();
+    function loginEmptyPass(AcceptanceTester $I, \Page\Login $loginPage) {
+        $loginPage->login('cadence.test01@yahoo.com', '');
+        $I->see( 'This is a required field.','#advice-required-entry-pass');
+        $I->comment('Expected result: This is a required field pass.');
+    }
 
-        $guestPage->payPalSite();
-        $I->comment('Expected result: You are have your order in PayPal');
+    function loginEmptyEmail(AcceptanceTester $I, \Page\Login $loginPage) {
+        $loginPage->login('', '123456');
+        $I->see( 'This is a required field.','#advice-required-entry-email');
+        $I->comment('Expected result: This is a required field email.');
+    }
 
+    function loginInvalidEmail(AcceptanceTester $I, \Page\Login $loginPage) {
+        $loginPage->login('testmail.ru', '123456');
+        $I->see('Please enter a valid email address. For example johndoe@domain.com.', '#advice-validate-email-email');
+        $I->comment('Expected result: Please enter a valid email address.');
+    }
+
+    function loginWrongEmail(AcceptanceTester $I, \Page\Login $loginPage) {
+        $loginPage->login('test@test.ru', '123456');
+        $I->see('Invalid login or password.', 'li.error-msg');
+        $I->comment('Expected result: Please enter a valid email address.');
     }
 }
