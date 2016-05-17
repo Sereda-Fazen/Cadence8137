b2fc74e4f46d188a331d0604a198b508fb1a1cd7
Sereda-Fazen <fazen7@mail.ru>
2016-05-17 13:53:27 +0300

    devWhole-9

--- tests/acceptance/00_TestCest.php
+++ tests/acceptance/00_TestCest.php
@@ -8,16 +8,10 @@ use Step\Acceptance;
 class TestCest
 {
 
-    function checkGiftCardForUser(Step\Acceptance\UserSteps  $I)
+    function registerSuccess(\Page\Register $registerPage, \Step\Acceptance\LoginSteps $I)
     {
-        $I->stepsLoginIn();
-        $I->comment('Expected result: You are entering in your account ');
-
-        $I->userProcessCheckout();
-        $I->comment('Expected result: Show your addresses');
-
-        $I->checkGiffCard();
-        $I->comment('Expected result: Showing 1 products which in processing');
+        $registerPage->register('alex', 'sereda', 'cadence_watch@yahoo.com', '123456', '123456');
+        $I->checkExistUser();
+        $registerPage->logout();
     }
-
 }
